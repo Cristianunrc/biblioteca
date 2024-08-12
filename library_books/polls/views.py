cbcdb306ec84a2c90fcf6451fad85af75eaec42b
Cristian <cristiankhan23@gmail.com>
2024-08-12 08:58:13 -0300

    Render index and add views

--- library_books/polls/views.py
+++ library_books/polls/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello to the library books!")
+  return render(request, "views/index.html")
 
 def about(request):
   return render(request, "views/about.html")
 
 def books(request):
   return render(request, "books/index.html")
+
+def create_book(request):
+  return render(request, "books/add.html")
