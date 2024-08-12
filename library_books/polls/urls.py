4e5454ce9ee7c79eb1e1d223ec5af015ee75bd65
Cristian <cristiankhan23@gmail.com>
2024-08-12 08:58:53 -0300

    Path to add view

--- library_books/polls/urls.py
+++ library_books/polls/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path(route name, function name, file html name)
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("books/", views.books, name="books"),
+    path("books/add", views.create_book, name="add"),
 ]
