cf864c847a5b1fd35921e4300b4c10f448398072
Cristian <cristiankhan23@gmail.com>
2024-08-14 11:55:54 -0300

    Add new def, delete's unnecesary imports and comment

--- library_books/polls/views.py
+++ library_books/polls/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 def index(request):
-  return render(request, "views/index.html")
+    return render(request, "views/index.html")
 
 def about(request):
-  return render(request, "views/about.html")
+    return render(request, "views/about.html")
 
 def books(request):
-  return render(request, "books/index.html")
+    return render(request, "books/index.html")
 
 def create_book(request):
-  return render(request, "books/add.html")
+    return render(request, "books/add.html")
+
+def edit_book(request):
+    return render(request, "books/edit.html")
