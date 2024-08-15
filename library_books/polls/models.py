7d93d4d645a2e15861c1c4b57aaf037a4cb480d2
Cristian <cristiankhan23@gmail.com>
2024-08-15 09:38:42 -0300

    Add constructor and delete function

--- library_books/polls/models.py
+++ library_books/polls/models.py
@@ -6,3 +6,11 @@ class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name="Title")
     image = models.ImageField(upload_to='images/', verbose_name="Image", null=True)
     description = models.TextField(verbose_name="Description", null=True)
+
+    def __str__(self):
+        row = "Title: " + self.title + " | " + "Description: " + self.description
+        return row
+    
+    def delete(self, using=None, keep_parents=False):
+        self.image.storage.delete(self.image.name)
+        super().delete
