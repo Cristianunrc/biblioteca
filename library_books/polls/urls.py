c48fc31a4dc82c8ddcd656d4b1e97ba4d54df901
Cristian <cristiankhan23@gmail.com>
2024-08-18 10:25:48 -0300

    Static files on urls

--- library_books/polls/urls.py
+++ library_books/polls/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 
 urlpatterns = [
     # path(route name, function name, file html name)
@@ -9,3 +12,5 @@ urlpatterns = [
     path("books/add", views.create_book, name="add"),
     path("books/edit", views.edit_book, name="edit"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
