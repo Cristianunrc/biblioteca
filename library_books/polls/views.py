a256ed7b6d78137ae39ca3329e3ef4d7a47e8e62
Cristian <cristiankhan23@gmail.com>
2024-08-18 12:04:09 -0300

    Logic for create a new book

--- library_books/polls/views.py
+++ library_books/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-
+from .forms import BookForm
 
 def index(request):
     return render(request, "views/index.html")
@@ -13,7 +13,8 @@ def books(request):
     return render(request, "books/index.html", {'books': books})
 
 def create_book(request):
-    return render(request, "books/add.html")
+    form = BookForm(request.POST or None)
+    return render(request, "books/add.html", {'form': form})
 
 def edit_book(request):
     return render(request, "books/edit.html")
