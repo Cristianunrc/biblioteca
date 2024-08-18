bcf5b21fbbfa6fca13902270a1984420810d8d4e
Cristian <cristiankhan23@gmail.com>
2024-08-18 10:27:27 -0300

    Change field on image and create migration

--- library_books/polls/models.py
+++ library_books/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, verbose_name="Title")
-    image = models.ImageField(upload_to='images/', verbose_name="Image", null=True)
+    image = models.ImageField(upload_to='', verbose_name="Image", null=True)
     description = models.TextField(verbose_name="Description", null=True)
 
     def __str__(self):
